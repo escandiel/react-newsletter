b061edfacb6ba7a4bb76242bb1ac2b212c8b8318
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-14 19:32:32 -0300

    first styles with tailwind css in the HeaderJsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <div className="flex">
-      <span>Ola</span>
-      <h1>Newsletter</h1>
-      <input type="checkbox" />
+    <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+      <span className="text-gray-100">Ola</span>
+      <h1 className="text-gray-100 text-xl">Newsletter</h1>
+      <input className="hidden sm:block" type="checkbox" />
     </div>
   );
 };
