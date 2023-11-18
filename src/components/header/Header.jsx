6e201ef2181358933792a458d206ead9ef65323d
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-17 22:21:45 -0300

    adding darkMode to tailwind.config.cjs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Ola</span>
       <h1 className="text-gray-100 text-xl">Newsletter</h1>
-      <MoonIcon className="h-8 text-gray-100 hidden sm:block" />
+      <MoonIcon className="h-8 text-gray-100 hidden sm:block cursor-pointer" />
     </div>
   );
 };
